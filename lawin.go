183c92a1049d6006eab570762291cf131f2acd34
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2019-08-30 11:08:46 +0800

    add GET & POST methods

--- lawin.go
+++ lawin.go
@@ -1,6 +1,7 @@
 package lawin
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -8,7 +9,7 @@ import (
 )
 
 // TransformStrToMap : To string to json map
-// jsonstr - Json String
+// jsonstr - json String
 func TransformStrToMap(jsonstr string) map[string]interface{} {
 	var jsonMap map[string]interface{}
 	json.Unmarshal([]byte(jsonstr), &jsonMap)
@@ -25,4 +26,61 @@ func ExtractRespBody(resp *http.Response) string {
 	defer resp.Body.Close()
 	bodyString := string(bodyBytes)
 	return bodyString
+}
+
+// HTTPGet : http GET Request
+// URL - http URL
+// queryString - query string params
+// headers - request headers
+func HTTPGet(URL string, queryString map[string]interface{},
+	headers map[string]string) *http.Response {
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", URL, nil)
+	if headers != nil {
+		for key, value := range headers {
+			req.Header.Add(key, value)
+		}
+	}
+
+	if queryString != nil {
+		q := req.URL.Query()
+		for key, value := range queryString {
+			strValue, _ := value.(string)
+			q.Add(key, strValue)
+		}
+		req.URL.RawQuery = q.Encode()
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("HTTP GET Failed")
+	}
+	return resp
+}
+
+// HTTPPost : HTTP Post request
+// URL - http URL
+// headers - request headers
+func HTTPPost(URL string, payload map[string]interface{},
+	headers map[string]string) *http.Response {
+	client := &http.Client{}
+
+	jsonPayload, _ := json.Marshal(payload)
+	jsonString := string(jsonPayload)
+
+	fmt.Println(jsonString)
+
+	req, _ := http.NewRequest("POST", URL, bytes.NewBuffer(jsonPayload))
+	if headers != nil {
+		for key, value := range headers {
+			req.Header.Add(key, value)
+		}
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("HTTP POST Failed")
+	}
+
+	return resp
 }
